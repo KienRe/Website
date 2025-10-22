7b5decd3650012993d42d3e97bfec40ff2b9671a
Rene <rene102@live.de>
2025-10-22 15:10:59 +0200

    Added another blog post

--- blog-data.js
+++ blog-data.js
@@ -3,11 +3,19 @@ const BLOG_POSTS = [
   {
     id: "reading-metadata",
     date: "December 2024",
-    title: "Reading Metadata Specifiers in a Packaged Build in Unreal",
+    title: "Reading metadata specifiers in a packaged Unreal Build",
     excerpt:
       "One common challenge in Unreal Engine development is accessing metadata specifiers in a packaged build. This post walks through a robust solution using commandlets, runtime dependencies, and non-asset directories to export, package, and load metadata automatically.",
-    link: "reading-metadata.html",
+    link: "blog/reading-metadata.html",
   },
+  {
+    id: "steam-upload-tool",
+    date: "December 2025",
+    title: "Building a quick one click deploy to Steam tool in Slate",
+    excerpt:
+      "Uploading builds to Steam typically involves running SteamCMD from the command line with various parameters. For teams working in Unreal Engine, having a custom editor toothat streamlines this process can save time and reduce errors.",
+    link: "blog/steam-upload-tool.html",
+  }
   // Add more blog posts here:
   // {
   //   id: 'my-new-post',
